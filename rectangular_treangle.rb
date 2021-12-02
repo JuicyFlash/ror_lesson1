f1a0a2782ca7f6d921661d778b42bc966b11c6cc
JuicyFlash <tankedet@bk.ru>
2021-12-02 23:30:12 +0300

    Задача прямоугольный треугольник

---
+++ rectangular_treangle.rb
@@ -0,0 +1,26 @@
+print "Задайте сторону a = "
+a = gets.chomp.to_f
+print "Задайте сторону b = "
+b = gets.chomp.to_f
+print "Задайте сторону c = "
+c = gets.chomp.to_f
+#определяем самую длинную сторону
+if (a > b) && (a > c)
+is_rectangular = a**2 == (b**2 + c**2)
+elsif (b > a) && (b > c)
+is_rectangular = b**2 == (a**2 + c**2)
+elsif (c > a) && (c > b)
+is_rectangular = c**2 == (b**2 + a**2)
+end
+
+if (a == b) && (b == c)
+puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} равносторонний"
+elsif (a==b) || (b == c) || (c == a) 
+print "Треугольник со сторонами a=#{a} b=#{b} c=#{c} равнобедренный"  
+puts " и прямоугольный" if is_rectangular
+puts if !is_rectangular
+elsif is_rectangular
+puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} прямоугольный"  
+elsif !is_rectangular
+puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} не является прямоугольным, равнобедренным, или равносторонним" 
+end
