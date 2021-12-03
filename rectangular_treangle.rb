6fc2524e3d710d70b16a5329b972d15b91a844fa
JuicyFlash <tankedet@bk.ru>
2021-12-03 18:18:46 +0300

    прямоугольный треугольник - добавлены отступы

--- rectangular_treangle.rb
+++ rectangular_treangle.rb
@@ -6,21 +6,21 @@ print "Задайте сторону c = "
 c = gets.chomp.to_f
 #определяем самую длинную сторону
 if (a > b) && (a > c)
-is_rectangular = a**2 == (b**2 + c**2)
+  is_rectangular = a**2 == (b**2 + c**2)
 elsif (b > a) && (b > c)
-is_rectangular = b**2 == (a**2 + c**2)
+  is_rectangular = b**2 == (a**2 + c**2)
 elsif (c > a) && (c > b)
-is_rectangular = c**2 == (b**2 + a**2)
+  is_rectangular = c**2 == (b**2 + a**2)
 end
 
 if (a == b) && (b == c)
-puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} равносторонний"
+  puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} равносторонний"
 elsif (a==b) || (b == c) || (c == a) 
-print "Треугольник со сторонами a=#{a} b=#{b} c=#{c} равнобедренный"  
-puts " и прямоугольный" if is_rectangular
-puts if !is_rectangular
+  print "Треугольник со сторонами a=#{a} b=#{b} c=#{c} равнобедренный"  
+  puts " и прямоугольный" if is_rectangular
+  puts if !is_rectangular
 elsif is_rectangular
-puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} прямоугольный"  
+  puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} прямоугольный"  
 elsif !is_rectangular
-puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} не является прямоугольным, равнобедренным, или равносторонним" 
+  puts "Треугольник со сторонами a=#{a} b=#{b} c=#{c} не является прямоугольным, равнобедренным, или равносторонним" 
 end
