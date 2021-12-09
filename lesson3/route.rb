0999a2274ba9c1f40d2fbbac3d24d07b9db2a105
JuicyFlash <tankedet@bk.ru>
2021-12-09 17:16:46 +0300

    Route.rb

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -4,33 +4,42 @@
 #Может удалять промежуточную станцию из списка
 #Может выводить список всех станций по-порядку от начальной до конечной
 class Route
-  def initialize (start_station, end_station)
-    @start_station = start_station
-	@end_station = end_station
-	# Сразу делаем массив, который потом будем изменять (добавлять\удалять) - это позволит изменять маршрут у поездов, которым он уже установлен
-	@route = [@start_station, @end_station]	
+  attr_reader :stations
+  
+  def initialize (start_station, end_station)   
+	# Сразу делаем массив, который потом будем изменять (добавлять\удалять) 
+	@stations = [start_station, end_station]	
   end
+  
   def add_station (station)
     #Не допускаем повторное добавление станций, в том чиле начальной и конечной
-    @route.insert(@route.length - 1, station) if (@route.include?(station) == false)
+    @stations.insert(@stations.length - 1, station) unless @stations.include?(station)
   end
+  
   def remove_station (station)
     #Удаляем станцию из маршрута. Не допускается удаление станции, если на ней находится поезд с этим маршрутом. Так же запрещаем удалять начальную и конечную станцию
-    @route.delete(station) if !(station.trains.map{|tr| tr.route}.include?(self)) && ((station !=@start_station) && (station != @start_station))
+    @stations.delete(station) if !(station.trains.map{|tr| tr.route}.include?(self)) && ((station !=@start_station) && (station != @start_station))
   end
-  def print_route
+  
+  def print_stations
     #Выводим список всех станций по-порядку от начальной до конечной
     index = 0 
-	lght = @route.length
+	lght = @stations.length
 	puts ("Маршрут:")
-    @route.map{|st| 
+    @stations.map{|st| 
 	  print "#{st.name}(#{index+=1})" 
 	  print " -> " if index < lght
 	  }
 	puts
-  end
-  def get_route
-    #Возвращает весь маршрут как []
-    @route
+  end 
+  
+  def first_station
+  # Возвращает первую станцию
+    @stations[0]  
+  end 
+  
+  def last_station
+  # Возвращает последнюю станцию
+    @stations[-1]
   end   
 end
