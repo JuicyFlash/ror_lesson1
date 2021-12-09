956048e187cf847a30865669e78181c8c42177ac
JuicyFlash <tankedet@bk.ru>
2021-12-09 17:25:06 +0300

    Форматирование кода

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -6,17 +6,21 @@
 #Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
 class Station
   attr_reader :name
-  attr_reader :trains 
+  attr_reader :trains
+  
   def initialize (name)
     @name = name
 	@trains = []
   end
+  
   def recive_train (train)
     @trains << train
   end
+  
   def trains_by_type (train_type)
    @trains.select{|train| train.type == train_type}
   end
+  
   def send_train (train)
     @trains.delete(train)
   end
