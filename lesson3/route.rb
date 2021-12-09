930115dfd34cbe6d229cea73283728a7c05fd71f
JuicyFlash <tankedet@bk.ru>
2021-12-10 00:17:41 +0300

    Корректировка отступов

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -7,39 +7,39 @@ class Route
   attr_reader :stations
   
   def initialize (start_station, end_station)   
-  # Сразу делаем массив, который потом будем изменять (добавлять\удалять) 
-	@stations = [start_station, end_station]	
+    #Сразу делаем массив, который потом будем изменять (добавлять\удалять) 
+    @stations = [start_station, end_station]	
   end
   
   def add_station (station)
-  # Не допускаем повторное добавление станций, в том чиле начальной и конечной
+    #Не допускаем повторное добавление станций, в том чиле начальной и конечной
     @stations.insert(@stations.length - 1, station) unless @stations.include?(station)
   end
   
   def remove_station (station)
-  # Удаляем станцию из маршрута. Не допускается удаление станции, если на ней находится поезд с этим маршрутом. Так же запрещаем удалять начальную и конечную станцию
+    #Удаляем станцию из маршрута. Не допускается удаление станции, если на ней находится поезд с этим маршрутом. Так же запрещаем удалять начальную и конечную станцию
     @stations.delete(station) if !(station.trains.map{|tr| tr.route}.include?(self)) && ((station !=@start_station) && (station != @start_station))
   end
   
   def print_stations
-  # Выводим список всех станций по-порядку от начальной до конечной
+    #Выводим список всех станций по-порядку от начальной до конечной
     index = 0 
-	lght = @stations.length
-	puts ("Маршрут:")
+    lght = @stations.length
+    puts ("Маршрут:")
     @stations.map{|st| 
-	  print "#{st.name}(#{index+=1})" 
-	  print " -> " if index < lght
-	  }
-	puts
+    print "#{st.name}(#{index+=1})" 
+    print " -> " if index < lght
+    }
+    puts
   end 
   
   def first_station
-  # Возвращает первую станцию
+    #Возвращает первую станцию
     @stations[0]  
   end 
   
   def last_station
-  # Возвращает последнюю станцию
+    #Возвращает последнюю станцию
     @stations[-1]
-  end   
+  end
 end
--- lesson3/train.rb
+++ lesson3/train.rb
@@ -9,73 +9,73 @@
 #При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
 #Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
 #Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
- class Train
-   attr_reader :speed
-   attr_reader :vans_count
-   attr_reader :current_station
-   attr_reader :type
-   attr_reader :route
+class Train
+  attr_reader :speed
+  attr_reader :vans_count
+  attr_reader :current_station
+  attr_reader :type
+  attr_reader :route
+  
+  def initialize (number, type, vans_count)
+    @number = number
+    @type = type
+    @vans_count = vans_count
+    @speed = 0
+  end
    
-   def initialize (number, type, vans_count)
-     @number = number
-	 @type = type
-	 @vans_count = vans_count
-     @speed = 0	 
-   end
+  #Торможение до нуля
+  def speed_down
+    @speed = 0 
+  end
    
-   # Торможение до нуля
-   def speed_down     
-	@speed = 0 
-   end
-   
-   # Набор скорости
-   def speed_up (speed)
+  #Набор скорости
+  def speed_up (speed)
     @speed += speed
-   end
+  end
    
-   # Добавление вагонов в состав
-   def hook_van
-     vans_count += 1 if @speed == 0
-   end
+  #Добавление вагонов в состав
+  def hook_van
+    vans_count += 1 if @speed == 0
+  end
    
-   def unhook_van
-     vans_count -= 1 if (@speed == 0) && (@vans_count > 0 )
-   end
+  def unhook_van
+    vans_count -= 1 if (@speed == 0) && (@vans_count > 0 )
+  end
    
-   # Установка маршрута
-   def set_route (route)
+  #Установка маршрута
+  def set_route (route)
     @route = route
     @current_station = @route.stations[0]
-	@current_station.recive_train(self)
-	#self.recalc_stations
+  @current_station.recive_train(self)
+  #self.recalc_stations
+  end
+   
+  #Перемещение    
+  #При перемещении у текущей станции трегрится команда на отправку текущего поезда, а у следующей/предыдущей станции на получение 
+  def move_next_station
+    unless self.next_station.nil?
+      @current_station.send_train(self)
+      self.next_station.recive_train(self)
+      @current_station = self.next_station
+    end
+  end
+   
+  def move_prev_station
+    unless self.prev_station.nil?
+      @current_station.send_train(self)
+      self.prev_station.recive_train(self)
+      @current_station = self.prev_station
+    end
    end
    
-   # Перемещение    
-   # При перемещении у текущей станции трегрится команда на отправку текущего поезда, а у следующей/предыдущей станции на получение 
-   def move_next_station
-     unless self.next_station.nil?
-       @current_station.send_train(self)
-	   self.next_station.recive_train(self)
-	   @current_station = self.next_station
-	 end
-   end
+  #Следующая/предыдущая станция
+  def next_station
+    idx = @route.stations.index(@current_station)
+    @route.stations[idx + 1] unless @current_station == @route.last_station
+  end
    
-   def move_prev_station
-     unless self.prev_station.nil?
-       @current_station.send_train(self)
-	   self.prev_station.recive_train(self)
-	   @current_station = self.prev_station
-	 end   
-   end
-   
-   # Следующая/предыдущая станция
-   def next_station
-     idx = @route.stations.index(@current_station)
-	 @route.stations[idx + 1] unless @current_station == @route.last_station      
-   end
-   
-   def prev_station
-     idx = @route.stations.index(@current_station)
-     @route.stations[idx - 1] unless @current_station == @route.first_station
-   end   
- end
+  def prev_station
+    idx = @route.stations.index(@current_station)
+    @route.stations[idx - 1] unless @current_station == @route.first_station
+  end
+end
