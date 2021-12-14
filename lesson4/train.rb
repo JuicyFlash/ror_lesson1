5d85aa4f2a000c805162546b0a5a159a3cf7ca95
JuicyFlash <tankedet@bk.ru>
2021-12-14 23:02:49 +0300

    Базовый класс для CargoTrain и PassangerTrain

--- lesson4/train.rb
+++ lesson4/train.rb
@@ -1,45 +1,42 @@
 #Класс Train (Поезд):
-#Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
-#Может набирать скорость
-#Может возвращать текущую скорость
-#Может тормозить (сбрасывать скорость до нуля)
-#Может возвращать количество вагонов
-#Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов). Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-#Может принимать маршрут следования (объект класса Route). 
-#При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-#Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
-#Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
 class Train
   attr_reader :speed
   attr_reader :wagons
   attr_reader :current_station
   attr_reader :type
   attr_reader :route
+  attr_reader :number
   
-  def initialize (number, type)
+  def initialize (number)
     @number = number
-    @type = type
-    @vans_count = vans_count
     @speed = 0
+    @wagons = []
   end
-   
-  #Торможение до нуля
-  def speed_down
-    @speed = 0 
+  def print_type
+    if @type == :cargo
+      "грузовой"
+    elsif  @type == :passenger
+      "пассажирский"
+    end
   end
-   
-  #Набор скорости
-  def speed_up (speed)
-    @speed += speed
-  end
+
+
    
   #Добавление вагонов в состав
-  def hook_van (wagon)
-    wagons << wagon if @speed == 0
+  def hook_wagon (wagon)
+    if (@speed == 0) && (wagon.type == @type)
+      wagon.hooked = true
+      @wagons << wagon
+    end
+
   end
    
-  def unhook_van(wagon)
-    wagons.delete(wagon) if (@speed == 0) && (@vans_count > 0 )
+  def unhook_wagon(wagon)
+    if (@speed == 0) && (@wagons.length > 0 )
+      wagon.hooked = false
+      @wagons.delete(wagon)
+    end
+
   end
    
   #Установка маршрута
@@ -76,5 +73,17 @@ class Train
   def prev_station
     idx = @route.stations.index(@current_station)
     @route.stations[idx - 1] unless @current_station == @route.first_station
+  end
+
+  protected
+
+  #Торможение до нуля
+  def speed_down
+    @speed = 0
+  end
+
+  #Набор скорости
+  def speed_up (speed)
+    @speed += speed
   end
 end
