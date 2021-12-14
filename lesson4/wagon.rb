186f632f0bbb7eb37c97e9545748134942b7c1ca
JuicyFlash <tankedet@bk.ru>
2021-12-14 23:07:16 +0300

    Базовый класс для вагонов

--- lesson4/wagon.rb
+++ lesson4/wagon.rb
@@ -1,16 +1,11 @@
 #require_relative 'passenger_wagon'
 class Wagon
-  attr_accessor :hooked 
-  def initialize    
-    set_type
-  end  
-  attr_accessor :type
-  private
-  def set_type    
+  attr_accessor :hooked
+  attr_reader :type
+  attr_reader :type_for_print
+  def initialize
+    @hooked = false
   end
-end
-class PassengerWagon < Wagon    
-  def set_type
-    @type = :xxx
-  end
+  #Запрещаем создание объекта класса Wagon
+  private_class_method :new
 end
