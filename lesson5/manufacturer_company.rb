35fbfffa653d30e580759c2e4b718b6bc235edb7
JuicyFlash <tankedet@bk.ru>
2021-12-21 02:06:38 +0300

    Удалён лишний код, остался один аксессор

--- lesson5/manufacturer_company.rb
+++ lesson5/manufacturer_company.rb
@@ -1,16 +1,5 @@
 module ManufacturerCompany
 
-  def  set_manufacturer_company
-    puts "Введите компанию производителя: "
-    self.manufacturer_company = gets.chomp
-  end
-
-  def  get_manufacturer_company
-    self.manufacturer_company
-  end
-
-  protected
-
   attr_accessor  :manufacturer_company
 
 end
