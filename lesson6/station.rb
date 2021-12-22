0be02d0c4a4cc9c9bee850b73d3046854bac1dd6
JuicyFlash <tankedet@bk.ru>
2021-12-22 22:27:22 +0300

    Валидация Station

--- lesson6/station.rb
+++ lesson6/station.rb
@@ -8,6 +8,8 @@ class Station
 
   @@instances_array = []
 
+  NAME_FORMAT = /^[A-Z0-9]{1}\w+$/
+
   def self.all
     if @@instances_array.length == 0
       nil
@@ -17,8 +19,16 @@ class Station
 
   end
 
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
   def initialize (name)
     @name = name
+    validate!
     @trains = []
     self.register_instance
     @@instances_array << self
@@ -35,4 +45,11 @@ class Station
   def send_train (train)
     @trains.delete(train)
   end
+
+  protected
+  def validate!
+    raise "Не задано название станции" if @name.nil?
+    raise "Некорректное название станции #{NAME_FORMAT}" if @name !~ NAME_FORMAT
+  end
+
 end
