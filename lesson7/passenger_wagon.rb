bfd599b3933765faf8304b28549030c34a15a177
JuicyFlash <tankedet@bk.ru>
2021-12-24 17:52:51 +0300

    *Добавлен новый аттрибут согласно ТЗ*Доавлены упраявляющие методы*Добавлена валидация с использоавние исключений

--- lesson7/passenger_wagon.rb
+++ lesson7/passenger_wagon.rb
@@ -1,13 +1,46 @@
 class PassengerWagon < Wagon
 
-  def initialize
-    super
+  def initialize (seats)
+    @seats = seats.to_i
+    @unavailable_seats = 0
     @type = :passenger
     @type_for_print = "пассажирский"
+    super
   end
 
   def self.print_type
     "Пассажирский"
   end
+
+  def fill_space!
+    take_seat!
+  end
+
+  def available_space!
+    available_seats
+  end
+
+  def unavailable_space!
+    @unavailable_seats
+  end
+
+  protected
+
+  def take_seat!
+    if @seats > @unavailable_seats
+      @unavailable_seats += 1
+    else
+      raise "Заняты все места"
+    end
+  end
+
+  def available_seats
+    @seats - @unavailable_seats
+  end
+  def validate!
+    super
+    raise "Количестов мест должно быть больше 0" if @seats <= 0
+  end
+
 
 end
--- lesson7/wagon.rb
+++ lesson7/wagon.rb
@@ -5,7 +5,27 @@ class Wagon
   attr_accessor :hooked
   attr_reader :type
   attr_reader :type_for_print
-  def initialize
+
+  def initialize (param)
+    validate!
     @hooked = false
   end
+
+  def fill_space!
+    raise "У нетипизированных вагонов данный метод недоступен"
+  end
+
+  def unavailable_space!
+    raise "У нетипизированных вагонов данный метод недоступен"
+  end
+
+  def available_space!
+    raise "У нетипизированных вагонов данный метод недоступен"
+  end
+
+  protected
+  def validate!
+    raise "Запрещено создавать вагоны неопределённого типа" if self.class == Wagon
+  end
+
 end
